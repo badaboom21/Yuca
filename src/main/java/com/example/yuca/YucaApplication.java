45316782ec3d446b31beda68fc71a677634f1fcf
badaboom21 <130473771+badaboom21@users.noreply.github.com>
2026-05-22 16:41:55 +0200

    start project

---
+++ src/main/java/com/example/yuca/YucaApplication.java
@@ -0,0 +1,13 @@
+package com.example.yuca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YucaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YucaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yuca/YucaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yuca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YucaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
