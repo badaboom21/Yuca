8e0679184a410284b8bd7372e5655209762327f4
badaboom21 <130473771+badaboom21@users.noreply.github.com>
2026-05-22 17:02:47 +0200

    ajout des entités Catégorie, Marque et Produit

--- src/main/java/com/example/yuca/bdd/entity/Marque.java
+++ src/main/java/com/example/yuca/bdd/entity/Marque.java
@@ -1,15 +1,15 @@
 package com.example.yuca.bdd.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "produit")
-public class Produit {
+@Table(name = "marque")
+public class Marque {
 
     @Id
-    @GeneratedValue
-    private long id;
+    @Column(columnDefinition = "varchar(50)")
+    private String nom;
 }
